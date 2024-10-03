c1daf16be9a4cfa929d8c4613cd3876925c6fb49
Eduardo Emeregildo <emeregildoeduardo@gmail.com>
2024-10-03 11:51:53 -0400

    wsl test

--- emit.py
+++ emit.py
@@ -25,5 +25,11 @@ class Environment:
         #environment is global scope. New environments will be nested dictionaries. This way you have a way to traverse back in scope(i.e. use a global variable from a function's scope). Any variables that are more deeply nested than the current environment will be out of scope and thus not checked.
         #Best approach may be to implement a spaghetti stack, with each node having a dictionary of variables declared in that scope. 
         self.environment = {}
+        
+        def add_node(self,parent):
+            pass
+        
+        def find_ancestor(self,node):
+            pass
     
 
